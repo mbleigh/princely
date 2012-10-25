53c5ed6eaf040e5832c451ba90893ded9e980db8
Jonathan Barket <jbarket@sleepunit.com>
2012-10-25 17:07:35 +0000

    modified stylesheet_path to work with asset pipeline

--- lib/princely/pdf_helper.rb
+++ lib/princely/pdf_helper.rb
@@ -1,41 +1,41 @@
 module PdfHelper
   require 'princely'
-  
+
   def self.included(base)
     base.class_eval do
       alias_method_chain :render, :princely
     end
   end
-  
+
   def render_with_princely(options = nil, *args, &block)
     if options.is_a?(Hash) && options.has_key?(:pdf)
       options[:name] ||= options.delete(:pdf)
-      make_and_send_pdf(options.delete(:name), options)      
+      make_and_send_pdf(options.delete(:name), options)
     else
       render_without_princely(options, *args, &block)
     end
-  end  
-    
+  end
+
   private
-  
+
   def make_pdf(options = {})
     options[:stylesheets] ||= []
     options[:layout] ||= false
     options[:template] ||= File.join(controller_path,action_name)
-    
+
     prince = Princely.new()
     # Sets style sheets on PDF renderer
     prince.add_style_sheets(*options[:stylesheets].collect{|style| stylesheet_file_path(style)})
-    
+
     html_string = render_to_string(:template => options[:template], :layout => options[:layout])
-    
+
     # Make all paths relative, on disk paths...
     html_string.gsub!(".com:/",".com/") # strip out bad attachment_fu URLs
     html_string.gsub!( /src=["']+([^:]+?)["']/i ) { |m| "src=\"#{Rails.public_path}/" + $1 + '"' } # re-route absolute paths
-    
+
     # Remove asset ids on images with a regex
     html_string.gsub!( /src=["'](\S+\?\d*)["']/i ) { |m| 'src="' + $1.split('?').first + '"' }
-    
+
     # Send the generated PDF file from our html string.
     if filename = options[:filename] || options[:file]
       prince.pdf_from_string_to_file(html_string, filename)
@@ -49,11 +49,20 @@ module PdfHelper
       make_pdf(options),
       :filename => pdf_name + ".pdf",
       :type => 'application/pdf'
-    ) 
+    )
   end
-  
+
   def stylesheet_file_path(stylesheet)
-    stylesheet = stylesheet.to_s.gsub(".css","")
-    File.join(config.stylesheets_dir, "#{stylesheet}.css")
+    # yanked from wicked_pdf. man, that guy is awesome.
+    if Rails.configuration.assets.compile == false
+      if ActionController::Base.asset_host
+        # asset_path returns an absolute URL using asset_host if asset_host is set
+        asset_path(stylesheet)
+      else
+        File.join(Rails.public_path, asset_path(stylesheet))
+      end
+    else
+      Rails.application.assets.find_asset(stylesheet).pathname
+    end
   end
 end
