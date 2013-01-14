7b9d6af5a674c11baac8eb8719655527ece11fa3
Jared Fraser <xanatosredfern@gmail.com>
2013-01-14 22:11:42 +1100

    Source image file paths from the Asset Pipeline

--- lib/princely/pdf_helper.rb
+++ lib/princely/pdf_helper.rb
@@ -25,13 +25,13 @@ module PdfHelper
     
     prince = Princely.new()
     # Sets style sheets on PDF renderer
-    prince.add_style_sheets(*options[:stylesheets].collect{|style| stylesheet_file_path(style)})
+    prince.add_style_sheets(*options[:stylesheets].collect{|style| asset_file_path(style)})
     
     html_string = render_to_string(:template => options[:template], :layout => options[:layout])
     
     # Make all paths relative, on disk paths...
     html_string.gsub!(".com:/",".com/") # strip out bad attachment_fu URLs
-    html_string.gsub!( /src=["']+([^:]+?)["']/i ) { |m| "src=\"#{Rails.public_path}/" + $1 + '"' } # re-route absolute paths
+    html_string.gsub!( /src=["']+([^:]+?)["']/i ) { |m|  puts $1; "src=\"#{asset_file_path($1) || $1}\"" } # re-route absolute paths
     
     # Remove asset ids on images with a regex
     html_string.gsub!( /src=["'](\S+\?\d*)["']/i ) { |m| 'src="' + $1.split('?').first + '"' }
@@ -54,16 +54,20 @@ module PdfHelper
     ) 
   end
   
-  def stylesheet_file_path(stylesheet)
+  def asset_file_path(asset)
+    # remove /asset/ in generated paths
+    asset.gsub!(/\/assets\//, "")
+    
     if Rails.configuration.assets.compile == false
       if ActionController::Base.asset_host
         # asset_path returns an absolute URL using asset_host if asset_host is set
-        asset_path(stylesheet)
+        asset_path(asset)
       else
-        File.join(Rails.public_path, asset_path(stylesheet))
+        File.join(Rails.public_path, asset_path(asset))
       end
     else
-      Rails.application.assets.find_asset(stylesheet).pathname
+      Rails.application.assets.find_asset(asset).try(:pathname)
     end
   end
+  alias_method :stylesheet_file_path, :asset_file_path
 end
