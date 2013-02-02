f386d9c00ef0d90fcca427c8d2bfe175fa21a19e
Philip Arndt <parndt@gmail.com>
2013-02-02 20:11:38 +1300

    Move Princely::AssetSupport to its own file.

--- lib/princely/pdf_helper.rb
+++ lib/princely/pdf_helper.rb
@@ -56,24 +56,4 @@ module PdfHelper
       :disposition => options[:disposition]
     )
   end
-
-  module AssetSupport
-    def localize_html_string(html_string, asset_path = nil)
-      # Make all paths relative, on disk paths...
-      html_string.gsub!(".com:/",".com/") # strip out bad attachment_fu URLs
-      html_string.gsub!( /src=["']+([^:]+?)["']/i ) do |m|
-        asset_src = asset_path ? "#{asset_path}/#{$1}" : asset_file_path($1)
-        %Q{src="#{asset_src}"} # re-route absolute paths
-      end
-
-      # Remove asset ids on images with a regex
-      html_string.gsub!( /src=["'](\S+\?\d*)["']/i ) { |m| %Q{src="#{$1.split('?').first}"} }
-      html_string
-    end
-
-    def asset_file_path(asset)
-      # Remove /assets/ from generated names and try and find a matching asset
-      Rails.application.assets.find_asset(asset.gsub(%r{/assets/}, "")).try(:pathname) || asset
-    end
-  end
 end
