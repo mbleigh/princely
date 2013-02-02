d90162294ff7eb12e60ebfab8cc07b20a8ca9dbe
Philip Arndt <parndt@gmail.com>
2013-02-02 21:23:27 +1300

    Reorganise PdfHelper and AssetSupport into Princely namespace.

--- lib/princely/rails.rb
+++ lib/princely/rails.rb
@@ -4,5 +4,5 @@ if Mime::Type.lookup_by_extension(:pdf) != 'application/pdf'
   Mime::Type.register 'application/pdf', :pdf
 end
 
-ActionController::Base.send(:include, PdfHelper)
-ActionController::Base.send(:include, PdfHelper::AssetSupport) if Rails::VERSION::MINOR > 0
+ActionController::Base.send(:include, Princely::PdfHelper)
+ActionController::Base.send(:include, Princely::AssetSupport) if Rails::VERSION::MINOR > 0
