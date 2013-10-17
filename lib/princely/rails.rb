695ced669d414514797dfe83c7805abd9f7af55b
Jared Fraser <j.fraser@modsognir.com>
2013-10-17 10:27:15 +0100

    Asset support should be included for rails 4

--- lib/princely/rails.rb
+++ lib/princely/rails.rb
@@ -5,4 +5,6 @@ if Mime::Type.lookup_by_extension(:pdf) != 'application/pdf'
 end
 
 ActionController::Base.send(:include, PdfHelper)
-ActionController::Base.send(:include, PdfHelper::AssetSupport) if Rails::VERSION::MINOR > 0
+ActionController::Base.send(:include, PdfHelper::AssetSupport) if
+ (Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR > 0) ||
+ (Rails::VERSION::MAJOR >= 4)
