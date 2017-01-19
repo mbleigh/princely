3cd77ac08b27d12a056b10dd6571386a2c7767c8
Ronaldo de Sousa Araujo <ronaldo.araujo@claretiano.edu.br>
2017-01-19 08:52:44 -0200

    Fix travis fail

--- lib/princely/pdf.rb
+++ lib/princely/pdf.rb
@@ -72,7 +72,7 @@ module Princely
         pdf.close_write
         result = pdf.gets(nil)
         pdf.close_read
-        result.force_encoding('BINARY') if RUBY_VERSION >= "1.9"
+        result && result.force_encoding('BINARY') if RUBY_VERSION >= "1.9"
 
         result
       end
