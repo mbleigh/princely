a98a0b49db63c0dcb94c57dfbf9c6cdcd6f77715
Jamie Dyer <jamie@kernowsoul.com>
2019-07-05 11:18:02 +0100

    Fix: AssetSupport not getting called in rails 5.2

--- lib/princely/rails.rb
+++ lib/princely/rails.rb
@@ -5,12 +5,6 @@ if Mime::Type.lookup_by_extension(:pdf) != 'application/pdf'
 end
 
 if defined?(Rails)
-  if Rails::VERSION::MAJOR >= 5
-    ActionController::Base.send(:prepend, Princely::PdfHelper)
-  else
-    ActionController::Base.send(:include, Princely::PdfHelper)
-  end
-  ActionController::Base.send(:include, Princely::AssetSupport) if
-   (Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR > 0) ||
-   (Rails::VERSION::MAJOR >= 4)
+  ActiveSupport.on_load(:action_controller) { include Princely::PdfHelper }
+  ActiveSupport.on_load(:action_controller) { include Princely::AssetSupport }
 end
